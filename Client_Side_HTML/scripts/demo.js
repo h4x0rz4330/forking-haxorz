d115e6b8474a10275c7af8ed716660d02d7d0fb3
Ryan Ngo <rawrimryno@gmail.com>
2016-11-28 00:09:14 -0600

    last minute changes.

--- Client_Side_HTML/scripts/demo.js
+++ Client_Side_HTML/scripts/demo.js
@@ -7,34 +7,37 @@ var userWin = false;
 
 $(document).ready(function(){
     //gets the users initial window dimensions.
-    boardState.initializeGameDimensions();
-    playerStates.initPlayerState(2);
-    applyModalAnimation();
-    //ripples();
-    populateDeck();
-    $('#playerModal').on('hidden.bs.modal', function () {
+	boardState.initializeGameDimensions();
+	playerStates.initPlayerState(2);
+	applyModalAnimation();
+	//ripples();
+	populateDeck();
+	$('#playerModal').on('hidden.bs.modal', function () {
 
-    })
-    //this will be where the outter most loop will occur that will trigger each round
+	})
+	//this will be where the outter most loop will occur that will trigger each round
 
-        setupBoard();
-        //deal Phase
+	setupBoard();
+			
+    setTimeout(function(){
+		//deal Phase
 		  dealCards(2);
-        //updates the dimensions of board and performs correct corrections to maintain card position relative to gameboard.
-        $(window).resize(function(){
-            boardState.updateBoardDimensions();
-            playerStates.correctCards();
-        });
+		//updates the dimensions of board and performs correct corrections to maintain card position relative to gameboard.
+		$(window).resize(function(){
+			boardState.updateBoardDimensions();
+			playerStates.correctCards();
+		});
 
-        //This will be where main loop in each round that will trigger the start of a new turn
+		//This will be where main loop in each round that will trigger the start of a new turn
 
-            //Draw Phase
-            
+			//Draw Phase
+			
 
-            //Play Phase
+			//Play Phase
 				drawCard('p1');
 				//drawCard('p2');
-            //End of turn phase
+			//End of turn phase
+	},3000);
 });
 
 function cpPlayCard(){
